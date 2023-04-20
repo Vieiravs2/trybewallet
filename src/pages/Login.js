1404e6f4cd6caa03a0686a0c83aa919b0bad165a
Vieiravs2 <matheusemidio.canal@gmail.com>
2023-04-20 15:45:48 -0300

    Fix(Import): Corrigido import de CSS inexistente

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import addUser from '../redux/actions';
-import './Login.css';
 
 class Login extends React.Component {
   state = {
