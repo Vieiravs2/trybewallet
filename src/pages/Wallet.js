2bd66d93a80d75285006f09ae5cd159a37752b0d
Vieiravs2 <matheusemidio.canal@gmail.com>
2023-04-20 15:58:04 -0300

    Feat(Req02): Adicionado o email do usuário na página de carteira

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <p data-testid="email-field">{ email }</p>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
