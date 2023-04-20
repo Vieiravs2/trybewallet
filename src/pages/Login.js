810246a398ae72cb8514c4a89b2351fdd929ebdf
Vieiravs2 <matheusemidio.canal@gmail.com>
2023-04-20 15:38:49 -0300

    Feat(Re01): Adicionado página de login e redirecionamento

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,74 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import addUser from '../redux/actions';
+import './Login.css';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  verifyEmail = (validateEmail) => {
+    const verifyEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    return verifyEmail.test(validateEmail);
+  };
+
+  verifyPassword = (validatePassword) => {
+    const MIN = 6;
+    return (validatePassword.length >= MIN);
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    const { dispatch } = this.props;
+
+    return (
+      <section>
+        <form>
+          <input
+            type="email"
+            name="email"
+            value={ email }
+            data-testid="email-input"
+            onChange={ this.handleChange }
+            placeholder="Email"
+          />
+
+          <input
+            type="password"
+            name="password"
+            value={ password }
+            data-testid="password-input"
+            onChange={ this.handleChange }
+            placeholder="Senha"
+          />
+          <Link to="/carteira">
+            <button
+              type="button"
+              disabled={ !this.verifyEmail(email) || !this.verifyPassword(password) }
+              onClick={ () => dispatch(addUser(email)) }
+            >
+              Entrar
+            </button>
+          </Link>
+        </form>
+      </section>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect()(Login);
