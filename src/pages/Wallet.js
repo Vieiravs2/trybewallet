20d2ddb10e610f33a963de8a66a1c5c173d80109
Vieiravs2 <matheusemidio.canal@gmail.com>
2023-04-20 21:01:47 -0300

    Feat(Req02): Adicionado componente Header

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,22 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
-    const { email } = this.props;
     return (
-      <p data-testid="email-field">{ email }</p>
+      <div className="userInfos">
+        <Header />
+      </div>
     );
   }
 }
 
-const mapStateToProps = (state) => ({
-  email: state.user.email,
-});
-
-Wallet.propTypes = {
-  email: PropTypes.string.isRequired,
-};
-
-export default connect(mapStateToProps)(Wallet);
+export default Wallet;
